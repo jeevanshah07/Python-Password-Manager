4a4dfd7752ad1a594162960814ee6bbc91732efc
Jeevan Shah <jedimaster3284@gmail.com>
2020-12-10 11:46:46 -0500

    Update TODO and encrypt.py

--- encrypt.py
+++ encrypt.py
@@ -27,11 +27,6 @@ class RSA_Cipher:
     plaintext = rsa_decryption_cipher.decrypt(ciphertext,16)
     return b64decode(plaintext).decode()
 
-'''
+
 cipher = RSA_Cipher()
 cipher.generate_key(1024) #key length can be 1024, 2048 or 4096
-encryption = cipher.encrypt("hello world")
-print(encryption)
-encryption = cipher.decrypt(encryption)
-print(encryption)
-'''
